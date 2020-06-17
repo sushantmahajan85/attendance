07ad1c720d179208bf9a531e7e7218498836fc77
sushantmahajan85 <sushantmahajan85@gmail.com>
2020-06-18 03:33:01 +0530

    fjehfej

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-var express  = require("express"),
-    app      = express(),
-    mongoose = require("mongoose"),
-    bodyParser = require("body-parser");
+var express = require("express"),
+  app = express(),
+  mongoose = require("mongoose"),
+  bodyParser = require("body-parser");
 //     flash    = require("connect-flash"),
 //     session  = require("express-session");
 // app.use(flash());
 
-
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // app.use(require("express-session")({
 //     secret : "Blah Blah!!",
@@ -17,21 +16,23 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 var routes = require("./routes/routes.js");
 
-mongoose.connect("mongodb://localhost:27017/tff",{'useNewUrlParser': true},(error)=>{
-    if(!error){
-        console.log("Connection to db successful");
+mongoose.connect(
+  "mongodb://localhost:27017/tff",
+  { useNewUrlParser: true },
+  (error) => {
+    if (!error) {
+      console.log("Connection to db successful");
+    } else {
+      console.log("Error connecting to db");
     }
-    else{
-        console.log("Error connecting to db");
-    }
-});
+  }
+);
 
 var Team = require("./models/team.js");
 app.use(express.static(__dirname));
 
-app.use("/",routes);
+app.use("/", routes);
 
-
-app.listen(27017,function(){
-    console.log("Server Started!");
+app.listen(27017, function() {
+  console.log("Server Started!");
 });
